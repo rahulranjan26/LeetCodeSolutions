2edbb46ab66d4390203d76d30ebd4bffc5ee5812
Rahul Ranjan <42500879+rahulranjan26@users.noreply.github.com>
2024-07-12 13:45:19 +0530

    Time: 437 ms (30.77%) | Memory: 22.3 MB (45.22%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        lo = min(weights)
+        hi = sum(weights)
+        ans = 0
+        while lo<=hi:
+            mid = (lo+hi)//2
+            if self.checkForDays(weights,mid,days):
+                hi = mid-1
+                ans = mid
+            else:
+                lo = mid+1
+        
+        return ans
+    
+    def checkForDays(self, weights, target, targetDays):
+        days = 1
+        wt = 0
+        
+        for i in range(0,len(weights)):
+            wt = wt+weights[i]
+            
+            if weights[i]>target:
+                return False
+            
+            if wt > target:
+                days=days+1
+                wt = weights[i]
+        
+        if days<=targetDays:
+            return True
+        return False
+                
+        
