53b05392cb45392ecd8504c3019193c32bf7e8d9
Rahul Ranjan <42500879+rahulranjan26@users.noreply.github.com>
2024-07-16 13:16:18 +0530

    Time: 67 ms (89.62%) | Memory: 19.7 MB (70.44%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,22 +1,73 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
-        
+        s = ""
         ans = []
         for i in range(len(searchWord)):
-            patt = searchWord[:i+1]
-            temp = self.findIndex(products,patt,i)
-            ans.append(temp[:3])
+            s+=searchWord[i]
+            idx = self.findTheFirstIndex(products,s)
+            temp = []
+            for x in range(idx, min(len(products),idx+3)):
+                if products[x][:i+1]==s:
+                    temp.append(products[x])
+            ans.append(temp)
         return ans
     
     
-    def findIndex(self,products,patt,idx):
-        ans = []
+    def findTheFirstIndex(self,products,s):
+        lo = 0
+        hi = len(products)-1
         
-        for i in range(len(products)):
-            if patt==products[i][:idx+1]:
-                ans.append(products[i])
-        return ans
+        while lo<=hi:
+            mid = (lo+hi)//2
+            if products[mid]<s:
+                lo = mid+1
+            else:
+                hi = mid-1
+        return lo
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         products.sort()
+        
+#         ans = []
+#         for i in range(len(searchWord)):
+#             patt = searchWord[:i+1]
+#             temp = self.findIndex(products,patt,i)
+#             ans.append(temp[:3])
+#         return ans
+    
+    
+#     def findIndex(self,products,patt,idx):
+#         ans = []
+#         for i in range(len(products)):
+#             if patt==products[i][:idx+1]:
+#                 ans.append(products[i])
+#         return ans
         
             
         
