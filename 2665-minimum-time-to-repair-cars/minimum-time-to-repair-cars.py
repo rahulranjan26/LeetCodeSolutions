9452340ec4e83c87d2ba5aa9774b6cc697845a27
Rahul Ranjan <42500879+rahulranjan26@users.noreply.github.com>
2024-07-18 09:57:49 +0530

    Time: 605 ms (67.17%) | Memory: 20.2 MB (42.43%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -3,14 +3,15 @@ class Solution:
     def repairCars(self, ranks, cars):
         lo = 1
         hi = cars * cars * max(ranks)
-
+        ans = -1
         while lo <= hi:
             mid = (hi + lo) // 2
             if self.isPossible(ranks, cars, mid):
+                ans = mid
                 hi = mid - 1
             else:
                 lo = mid + 1
-        return lo
+        return ans
 
     def isPossible(self, ranks, cars, mid):
         totalCars = 0
