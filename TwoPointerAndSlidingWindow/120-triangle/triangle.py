5d012e34f7337ddf8b4a5021c55382c1d475672d
Rahul Ranjan <42500879+rahulranjan26@users.noreply.github.com>
2025-04-30 12:31:13 +0530

    Time: 1 ms (92.84%) | Memory: 19.2 MB (44.06%) - LeetSync

--- TwoPointerAndSlidingWindow/120-triangle/triangle.py
+++ TwoPointerAndSlidingWindow/120-triangle/triangle.py
@@ -1,22 +1,10 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = []
-        for row in triangle:
-            
-            temp = len(row)
-            temp2 = []
-            for i in range(temp):
-                temp2.append(0)
-            dp.append(temp2)
-        for i in range(len(dp)-1,-1,-1):
-            for j in range(len(dp[i])-1,-1,-1):
-                if i==len(dp)-1:
-                    dp[i][j] = triangle[i][j]
-                else:
-                    target1 = dp[i+1][j]
-                    target2 = dp[i+1][j+1]
-                    dp[i][j] = min(triangle[i][j]+target1,triangle[i][j]+target2)
-        return dp[0][0]
+        for i in range(len(triangle)-2, -1, -1):  # for each row in triangle (skipping the last),
+            for j in range(i+1):                  # loop through each element in the row
+                triangle[i][j] += min(triangle[i+1][j],    # minimum sum from coordinate (x+1, y)
+                                      triangle[i+1][j+1])  # minimum sum from coordinate (x+1, y+1)
+        return triangle[0][0]
 
 
         
