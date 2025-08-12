4b6adf34ff179c6145b85acc702ebcbeea11a5ed
Rahul Ranjan <42500879+rahulranjan26@users.noreply.github.com>
2025-08-12 12:19:09 +0530

    Time: 1818 ms (47.75%) | Memory: 18.3 MB (14.19%) - LeetSync

---
+++ TwoPointerAndSlidingWindow/1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -0,0 +1,52 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        dpf = self.findLIS(nums)
+        dpb = self.findLDS(nums)
+
+        x = 0
+        for i in range(len(dpf)):
+            if dpf[i]>1 and dpb[i]>1:
+                x = max(x, dpf[i]+dpb[i])
+        print(dpf,dpb,x)
+        # print(x,len(nums)-x+1)
+        return len(nums)-x+1
+    
+
+    def findLIS(self,nums):
+        dp = [1]*len(nums)
+
+        for i in range(0, len(nums)):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    dp[i]=max(dp[i],dp[j]+1)
+        return dp
+    
+
+    def findLDS(self,nums):
+        dp = [1]*len(nums)
+
+        for i in range(len(nums)-1, -1,-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]>nums[j]:
+                    dp[i]=max(dp[i],dp[j]+1)
+        return dp
+
+
+    
+
+
+    def findIdx(self,res,x):
+
+        lo = 0
+        hi - len(res)-1
+
+        while lo<=hi:
+            mid=(lo+hi)//2
+            if res[mid]==x:
+                return mid
+            elif res[mid]>x:
+                hi=mid-1
+            else:
+                lo=mid+1
+        return lo
+        
