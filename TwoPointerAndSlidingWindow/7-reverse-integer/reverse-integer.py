8719ddb1a43e5473ff7014835c16c3e299edf113
Rahul Ranjan <42500879+rahulranjan26@users.noreply.github.com>
2025-08-18 14:41:40 +0530

    Time: 42 ms (37.47%) | Memory: 17.7 MB (83.52%) - LeetSync

--- TwoPointerAndSlidingWindow/7-reverse-integer/reverse-integer.py
+++ TwoPointerAndSlidingWindow/7-reverse-integer/reverse-integer.py
@@ -1,11 +1,14 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        signed = -1 if x < 0 else 1
-        x = abs(x)
-        x = str(x)
-        x = x[::-1]
-        x = x.strip()
-        x = int(x) * signed
+        sign = -1 if x < 0 else 1
+        rev = int(str(abs(x))[::-1]) * sign
+        return rev if -(2**31) <= rev <= (2**31 - 1) else 0
+        # signed = -1 if x < 0 else 1
+        # x = abs(x)
+        # x = str(x)
+        # x = x[::-1]
+        # x = x.strip()
+        # x = int(x) * signed
 
-        return x if -2 ** 31 <= x <= 2 ** 31 - 1 else 0
+        # return x if -2 ** 31 <= x <= 2 ** 31 - 1 else 0
         
